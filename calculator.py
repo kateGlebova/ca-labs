e7ab37c2c843dc042ae42dfe9a44bd50db163827
Kate <kate.glebova97@gmail.com>
2017-04-13 15:24:23 +0300

    Separate functions

--- calculator.py
+++ calculator.py
@@ -6,48 +6,60 @@ EXERCISE_LEVELS = {
     'Hard': 1.725,
     'Non-Stop': 1.9
 }
-DIET_MODES = {
-    'Safe': 0.85,
-    'Quick': 0.75,
-    'Extra': 0.6
-}
+
+
+def male_bmr(weight, height, age):
+    """
+    Calculate and return a man's basal metabolic rate
+    :param weight: float | weight in kilograms
+    :param height: float | height in centimeters
+    :param age: int | age in years
+    :return: float or int | BMR or -1 if parameters are not valid
+    """
+    if weight <= 0 or height <= 0 or age <= 0:
+        return -1
+    return 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+
+
+def female_bmr(weight, height, age):
+    """
+    Calculate and return a woman's basal metabolic rate
+    :param weight: float | weight in kilograms
+    :param height: float | height in centimeters
+    :param age: int | age in years
+    :return: float or int | BMR or -1 if parameters are not valid
+    """
+    if weight <= 0 or height <= 0 or age <= 0:
+        return -1
+    return 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+
+
+def bmr_calculator(sex, weight, height, age):
+    """
+    Calculate a person's basal metabolic rate
+    :param sex: string | 'male' or 'female'
+    :param weight: float | weight in kilograms
+    :param height: float | height in centimeters
+    :param age: int | age in years
+    :return: float or int | BMR or -1 if parameters are not valid
+    """
+    BMR = {'male': male_bmr, 'female': female_bmr}
+    if sex not in ('male', 'female'):
+        return -1
+    return BMR[sex](weight, height, age)
 
 
 def calories_calculator(sex, weight, height, age, exercise_level):
     """
-    Calculate daily calorie needs for person with specified parameters and return
-    either person's daily calories or -1 if parameters are not valid.
+    Calculate daily calorie needs for person with specified parameters.
     :param sex: string | 'male' or 'female'
     :param weight: float | weight in kilograms
     :param height: float | height in centimeters
     :param age: int | age in years
     :param exercise_level: string | name of the exercise level
-    :return:
+    :return: float or int | person's daily calories or -1 if parameters are not valid
     """
-    if not (sex in ('male', 'female') and exercise_level in EXERCISE_LEVELS and weight > 0 and height > 0):
+    bmr = bmr_calculator(sex, weight, height, age)
+    if bmr == -1:
         return -1
-
-    if sex == 'male':
-        bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
-    else:
-        bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
-
     return EXERCISE_LEVELS[exercise_level] * bmr
-
-
-def losing_weight(sex, weight, height, age, exercise_level, mode):
-    """
-    Calculate daily calorie needs for person who wants to lose weight with specified parameters and return
-    either person's daily calories or -1 if parameters are not valid.
-    :param sex: string | 'male' or 'female'
-    :param weight: float | weight in kilograms
-    :param height: float | height in centimeters
-    :param age: int | age in years
-    :param exercise_level: string | name of the exercise level
-    :param mode: string | name of the diet mode
-    :return:
-    """
-    if not (mode in DIET_MODES):
-        return -1
-
-    return DIET_MODES[mode] * calories_calculator(sex, weight, height, age, exercise_level)
